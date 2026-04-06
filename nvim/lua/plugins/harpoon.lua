a91c086034cc5ee31bb21ffc6c97c4969201f39e
MunMun06 <beddestroyer06@gmail.com>
2026-04-06 10:02:26 +0700

    sync 06/04/2026 d/m/y

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -9,14 +9,14 @@ return {
         -- Keymaps (Inside config so they only load when Harpoon is ready)
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon Add File" })
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {desc = "Harpoon Menu"})
-        
+
         vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, {desc = "Slot 1"})
         vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end, {desc = "Slot 2"})
         vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end, {desc = "Slot 3"})
         vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end, {desc = "Slot 4"})
 
         -- Toggle previous & next buffers
-        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, {desc = "harpoon prev"}) 
+        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, {desc = "harpoon prev"})
         vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, {desc = "harpoon next"})
     end,
 }
